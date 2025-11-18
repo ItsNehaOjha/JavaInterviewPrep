a4d8fb8a561c900dde87e28503b1cc2fdfce221f
ItsNehaOjha <ojhaneha0028@gmail.com>
2025-11-18 20:04:21 +0530

    6th day

--- A_HomeWorks/Day_2HW/Pattern5.java
+++ A_HomeWorks/Day_2HW/Pattern5.java
@@ -2,6 +2,17 @@ package A_HomeWorks.Day_2HW;
 
 public class Pattern5 {
     public static void main(String[] args) {
-        int a = 10;
+        int n=5;
+         for(int row = 0; row<= n;row++){
+            for(int col = 0;col<=n;col++){
+                if(row< n -col){
+                    System.out.print("_");
+                }
+                if(row >= n-col){
+                    System.out.print("*");
+                }
+            }
+            System.out.println(" ");
+        }
     }
 }
